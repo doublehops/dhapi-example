611b5a72ef1bed2752275f72b1e0f34e608eab7a
Damien Buttler <damien@doublehops.com>
2023-10-29 20:40:53 +1100

    It's working.

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -11,9 +11,9 @@ func GetRecordCount(DB *sql.DB, q string, params []any) (int32, error) {
 		err error
 		row *sql.Rows
 	)
-	var cs struct {
-		Count int32
-	}
+	//var cs struct {
+	//	Count int32
+	//}
 
 	if params == nil {
 		row, err = DB.Query(q)
@@ -27,20 +27,55 @@ func GetRecordCount(DB *sql.DB, q string, params []any) (int32, error) {
 	}
 
 	// from https://stackoverflow.com/questions/59629440/sql-expected-3-destination-arguments-in-scan-not-1-in-golang
-	TRY THIS - https://stackoverflow.com/questions/17845619/how-to-call-the-scan-variadic-function-using-reflection
+	//TRY THIS - https://stackoverflow.com/questions/17845619/how-to-call-the-scan-variadic-function-using-reflection
 	columns, err := row.Columns()
+	colCount := len(columns)
+	values := make([]interface{}, colCount)
+	valuesPtrs := make([]interface{}, colCount)
 	//receiver := make([]interface{}, len(columns))
-	receiver := make([]*interface{}, len(columns))
+	//receiver := make([]*interface{}, len(columns))
 
 	if row.Next() {
-		err = row.Scan(receiver...)
-		if err != nil {
-			return 0, fmt.Errorf("unable to scan query into var")
-		}
-		fmt.Printf("receiver: %+v\n", receiver)
-	}
+		for i := range columns {
+			valuesPtrs[i] = &values[i]
 
-	return cs.Count, nil
+		}
+
+		row.Scan(valuesPtrs...)
+
+		rowCount := columns[0]
+		fmt.Printf(">>>> RowCount: %s\n", rowCount)
+
+		//count := columns[0]
+		//fmt.Printf("count type: %d\n", reflect.TypeOf(count))
+
+		count := values[0].(int64)
+
+		return int32(count), nil
+
+		//for i, _ := range columns {
+		//	val := values[i]
+
+		//b, ok := val.([]byte)
+		//var v interface{}
+		//if ok {
+		//	v = string(b)
+		//} else {
+		//	v = val
+		//}
+		//
+		//fmt.Printf("v type: %d\n", reflect.TypeOf(v))
+		//vl, ok := v.(int)
+		//fmt.Printf("vl type: %d\n", reflect.TypeOf(vl))
+		//if !ok {
+		//	return 0, fmt.Errorf("unable to convert count to int32")
+		//}
+		//
+		//return int32(vl), nil
+	}
+	//}
+	//
+	return 0, fmt.Errorf("unable to find count")
 }
 
 //func FormatWhereClauses(r []req.FilterRules) (string, []any) {
